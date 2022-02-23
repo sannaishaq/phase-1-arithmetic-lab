bf2e9223c7f7b4b86ab4d777abeb21cfe7b4f82a
Sanna Ishaq <sakinah.ishaq@gmail.com>
2022-02-23 09:38:26 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const num1= 31
+let num2=2 
+let multiply = num1 * num2 
+// let result = num1 * num2 
+//return result; 
 
-// Write your code here
+let random 
+random = Math.floor(Math.random() * 10) +1
+//return 
+
+// const mod
+const num3= 16 
+const num4 =6
+const mod = num3%num4
+//return mod; 
+
+let max = 20;
+Math.max(1, 2, 3, 4, 5, 20)
+//return max;
